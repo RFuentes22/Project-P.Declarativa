da152ed683afae8926d1a38565f46594f6cb095f
Sara Romero <00030716@uca.edu.sv>
2019-11-18 14:57:05 -0600

    Ejercicio 1

--- project.rkt
+++ project.rkt
@@ -1,4 +1,56 @@
 #lang racket
+
+;1
+;ejercicio 1
+
+;Si queremos integrar 5
+;Ejemplo input es (integral 5)
+
+;Si queremos integrar X
+;Ejemplo input es (integral 'x)
+
+;Si queremos integrar X^3
+;Ejemplo input es (integral '((1 3)))
+
+;Si queremos integrar 5*X^3 + 4*X^2 + 3*X^1
+;Ejemplo input es (integral '((5 3) (4 2) (3 1)))
+
+;Si queremos integrar 5^x
+;Ejemplo input es (integral '((5 'x)))
+
+;Si queremos integrar 5^(x^3)
+;Ejemplo de input es (integral '((5 ('((1 3))))))
+
+(define (integral L)
+  (cond
+    [(empty? L) (void)]
+    [(number? L) (display (string-append "Integral = " (number->string L) "x"))]
+    [(equal? L 'x) (display "(x^2)/2")]
+    [else (display "Integral = ")
+          (mostrar-int L)
+          ])
+  )
+
+;(string-append  (number->string (first (first L))))
+
+(define (mostrar-int L)
+  (cond
+    [(empty? L) (display "c")]
+    [(equal? (second (first L)) ''x) (display (string-append (number->string(first(first L))) "^x" "/ln" (number->string(first (first L))) "+c" ))]
+    [(list? (second (first L))) (display (mostrar-int (second (first L))))]
+    [else
+     (display "(")
+     (display (first (first L)))
+     (display (string-append "*x^" (number->string (+ (second (first L)) 1)) ")"))
+     (display "/")
+     (display (number->string(+ (second (first L)) 1)))
+     (display "+")
+     (mostrar-int (rest L))
+    ]
+  )
+)
+
+
 ;ecuacion recta
 ;(findEq '(x1 y1) '(x2 y2) numaevaluar)
 ;(findEq '(4 6) '(2 2) 6)
